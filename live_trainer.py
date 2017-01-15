d61ab953742a992080223bf882356043542ac55e
johannesfrimberger <johannes.frimberger@gmail.com>
2017-01-15 18:47:30 +0100

    ---/JoFr: Update comments in live_trainer.py

--- live_trainer.py
+++ live_trainer.py
@@ -36,7 +36,6 @@ app = Flask(__name__)
 model = None
 prev_image_array = None
 
-joy_steering = 0
 lp_steering_angle = 0
 auto_mode = True
 
@@ -55,65 +54,81 @@ def telemetry(sid, data):
     imgString = data["image"]
     image = Image.open(BytesIO(base64.b64decode(imgString)))
     image_array = process_image(np.asarray(image))
-
     transformed_image_array = image_array[None, :, :, :]
 
-    global joy_steering
+    # Define global variables
     global lp_steering_angle
     global auto_mode
 
+    # Init variables and copy previous state
+    joy_steering = 0.
     prev_auto_mode = auto_mode
 
+    # Read data from gamepad
     for e in event.get():
+        # Read left joystick data in left/right direction
         if e.type == JOYAXISMOTION and e.dict["axis"] == 0:
+            # Value has a range of -1 to 1 --> Multiply with maximum steering angle
             joy_steering = 0.8563391 * e.dict["value"]
+        # Toggle auto_mode for A and X Button
         elif e.type == JOYBUTTONDOWN and (e.dict["button"] == 0 or e.dict["button"] == 2):
             auto_mode = not auto_mode
             print("Switch auto_mode to {}".format(auto_mode))
+            # For X button additionally remove previously stored training data (no training requested)
             if e.dict["button"] == 2:
                 training_images[:] = []
                 training_steering[:] = []
+        # For B button reset steering low pass to 0
         elif e.type == JOYBUTTONDOWN and e.dict["button"] == 1:
-            joy_steering = 0
+            lp_steering_angle = 0
             print("Reset steering request to {}".format(joy_steering))
 
+    # Switch between autonomous and manual driving
     if auto_mode:
         steering_angle = float(model.predict(transformed_image_array, batch_size=1))
     else:
+        # Low Pass joystick input
         alpha = 0.3
-        # print("Request {}".format(joy_steering))
         lp_steering_angle = (1.0 - alpha) * lp_steering_angle + alpha * joy_steering
         steering_angle = lp_steering_angle
 
+        # Add images and steering angle to array
         training_images.append(image_array)
         training_steering.append(steering_angle)
 
-    if (auto_mode and not(prev_auto_mode)):
-        print("Start training")
+    # Train images if we switch from manual to automatic driving
+    if auto_mode and not prev_auto_mode:
+        # Check if there is data stored for training
         if len(training_images) > 0:
+            print("Start training")
+
+            # Shuffle data
             n_samples = len(training_images)
             ind = np.arange(n_samples)
             random.shuffle(ind)
+
+            # Convert data to np arrays
             img = np.asarray(training_images)
             st = np.asarray(training_steering)
+
+            # Remove data after copying it
             training_images[:] = []
             training_steering[:] = []
+
+            # Start training
             model.fit(img[ind], st[ind], batch_size=10, nb_epoch=2, verbose=1)
 
+            # Save model
             model_filename = "updated_model.json"
             weights_filename = model_filename.replace('json', 'h5')
-
             with open(model_filename, "w") as outfile:
                 json.dump(model.to_json(), outfile)
-
             model.save_weights(weights_filename)
 
-    # The driving model currently just outputs a constant throttle. Feel free to edit this.
+    # Set default throttle
     throttle = 0.5
-
-    #print(steering_angle, throttle)
-
     send_control(steering_angle, throttle)
+
 
 @sio.on('connect')
 def connect(sid, environ):
